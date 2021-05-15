e230239996e7e564c23a1360c5925a2376e1ad87
b.sherafati <behnoud.sherafati@gmail.com>
2021-05-15 16:34:37 +0430

    first commit adding requiered dependencies

---
+++ app/src/main/java/ir/behnoudsh/aroundus/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.behnoudsh.aroundus.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.behnoudsh.aroundus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
